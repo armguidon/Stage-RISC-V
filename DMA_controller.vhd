7bf8088956b9763e6c6c8b27e60769925c5403c7
azeggagh <azeggaght@yahoo.fr>
2019-02-19 17:49:58 +0100

    Update DMA_controller.vhd

--- DMA_controller.vhd
+++ DMA_controller.vhd
@@ -1,1 +1,16 @@
 
+  library ieee; 
+  use ieee.std_logic_1164.all; 
+  use ieee.std_logic_unsigned.all; 
+  use ieee.std_logic_arith.all;
+
+
+
+
+entity DMA_controller is (
+  generic ( 
+    adress_width = 32;
+    data_width   = 32;
+    counter      = 
+    
+  );
