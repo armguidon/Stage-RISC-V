f20d0bf03645d00171166d0890297e339a9f10e7
azeggagh <azeggaght@yahoo.fr>
2019-02-19 09:43:39 +0100

    Update random_interrupt.sv

--- random_interrupt.sv
+++ random_interrupt.sv
@@ -24,5 +24,80 @@ module random_interrupt (
     output logic          irq_id_we_o
 
 );
-  
-  
+// calss random pour le nombre de cycles 
+ class rand_irq_cycles;
+    rand int n;
+endclass : rand_irq_cycles
+ 
+ // class random pour I/D
+ 
+  class rand_irq_id;
+    rand int n;
+endclass : rand_irq_id
+ 
+ // les signaux intermediaires
+ 
+logic [31:0] irq_mode_q;
+logic        irq_random;
+logic  [4:0] irq_id_random;
+logic        irq_ack_random;
+logic        irq_monitor;
+logic  [4:0] irq_id_monitor;
+logic        irq_ack_monitor;
+
+ // routage
+ assign irq_ack_o = irq_ack_i;
+ 
+ // reset
+ always_ff @(posedge clk_i or negedge rst_ni)
+begin
+    if(~rst_ni) begin
+        irq_mode_q <= 0;
+    end else begin
+        irq_mode_q <= irq_mode_i;
+    end
+end
+ 
+ // les cases
+ 
+ always_comb
+begin
+    unique case (irq_mode_q)
+        RANDOM:                                // interruption aleatoire
+        begin
+         irq_o     = irq_random;
+         irq_id_o  = irq_id_random;
+        end
+
+        PC_TRIG:
+        begin                                 // triguer le pc sur une valeur choisi
+         irq_o     = irq_monitor;
+         irq_id_o  = irq_id_monitor;
+        end
+
+        default:                             // mode default, routage des entree sur les sorties
+        begin
+         irq_o     = irq_i;
+         irq_id_o  = irq_id_i;
+        end
+    endcase
+end
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
