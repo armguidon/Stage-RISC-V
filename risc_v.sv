c0304d1f75c1fa9093bf8c10ea2f2c632db37daf
azeggagh <azeggaght@yahoo.fr>
2019-02-19 10:38:40 +0100

    Update risc_v.sv

--- risc_v.sv
+++ risc_v.sv
@@ -227,14 +227,81 @@ localparam PERT_REGS = 15;
        .irq_pc_trig_i             (irq_pc_trig_interm)
 );
 
-  
-  
-  
-  
-  
-  
-  
-  
+//-------------------------------------------------------------------
+//-------------------------------------------------------------------
+  `ifndef VERILATOR
+   generate
+       if(SIMCHECKER) begin: ri5cy_simchecker
+          riscv_simchecker riscv_simchecker_i
+          (
+            .clk              ( RISCV_CORE.clk_i                                ),
+            .rst_n            ( RISCV_CORE.rst_ni                               ),
+
+            .fetch_enable     ( RISCV_CORE.fetch_enable_i                       ),
+            .boot_addr        ( RISCV_CORE.boot_addr_i                          ),
+            .core_id          ( RISCV_CORE.core_id_i                            ),
+            .cluster_id       ( RISCV_CORE.cluster_id_i                         ),
+
+            .instr_compressed ( RISCV_CORE.if_stage_i.fetch_rdata[15:0]         ),
+            .if_valid         ( RISCV_CORE.if_stage_i.if_valid                  ),
+            .pc_set           ( RISCV_CORE.pc_set                               ),
+
+
+            .pc               ( RISCV_CORE.id_stage_i.pc_id_i                   ),
+            .instr            ( RISCV_CORE.id_stage_i.instr                     ),
+            .is_compressed    ( RISCV_CORE.is_compressed_id                     ),
+            .id_valid         ( RISCV_CORE.id_stage_i.id_valid_o                ),
+            .is_decoding      ( RISCV_CORE.id_stage_i.is_decoding_o             ),
+            .is_illegal       ( RISCV_CORE.id_stage_i.illegal_insn_dec          ),
+            .is_interrupt     ( RISCV_CORE.is_interrupt                         ),
+            .irq_no           ( RISCV_CORE.irq_id_i                             ),
+            .pipe_flush       ( RISCV_CORE.id_stage_i.controller_i.pipe_flush_i ),
+            .irq_i            ( RISCV_CORE.irq_i                                ),
+            .is_mret          ( RISCV_CORE.id_stage_i.controller_i.mret_insn_i  ),
+
+            .int_enable       ( RISCV_CORE.id_stage_i.m_irq_enable_i            ),
+
+            .lsu_ready_wb     ( RISCV_CORE.lsu_ready_wb                         ),
+            .apu_ready_wb     ( RISCV_CORE.apu_ready_wb                         ),
+            .wb_contention    ( RISCV_CORE.ex_stage_i.wb_contention             ),
+
+            .apu_en_id        ( RISCV_CORE.id_stage_i.apu_en                    ),
+            .apu_req          ( RISCV_CORE.ex_stage_i.apu_req                   ),
+            .apu_gnt          ( RISCV_CORE.ex_stage_i.apu_gnt                   ),
+            .apu_valid        ( RISCV_CORE.ex_stage_i.apu_valid                 ),
+            .apu_singlecycle  ( RISCV_CORE.ex_stage_i.apu_singlecycle           ),
+            .apu_multicycle   ( RISCV_CORE.ex_stage_i.apu_multicycle            ),
+            .apu_latency      ( RISCV_CORE.ex_stage_i.apu_lat_i                 ),
+            .apu_active       ( RISCV_CORE.ex_stage_i.apu_active                ),
+            .apu_en_ex        ( RISCV_CORE.ex_stage_i.apu_en_i                  ),
+
+            .ex_valid         ( RISCV_CORE.ex_valid                             ),
+            .ex_reg_addr      ( RISCV_CORE.id_stage_i.registers_i.waddr_b_i     ),
+
+            .ex_reg_we        ( RISCV_CORE.id_stage_i.registers_i.we_b_i        ),
+            .ex_reg_wdata     ( RISCV_CORE.id_stage_i.registers_i.wdata_b_i     ),
+
+            .ex_data_req      ( RISCV_CORE.data_req_o                           ),
+            .ex_data_gnt      ( RISCV_CORE.data_gnt_i                           ),
+            .ex_data_we       ( RISCV_CORE.data_we_o                            ),
+            .ex_data_addr     ( RISCV_CORE.data_addr_o                          ),
+            .ex_data_wdata    ( RISCV_CORE.data_wdata_o                         ),
+
+            .lsu_misaligned   ( RISCV_CORE.data_misaligned                      ),
+            .wb_bypass        ( RISCV_CORE.ex_stage_i.branch_in_ex_i            ),
+
+            .wb_valid         ( RISCV_CORE.wb_valid                             ),
+            .wb_reg_addr      ( RISCV_CORE.id_stage_i.registers_i.waddr_a_i     ),
+            .wb_reg_we        ( RISCV_CORE.id_stage_i.registers_i.we_a_i        ),
+            .wb_reg_wdata     ( RISCV_CORE.id_stage_i.registers_i.wdata_a_i     ),
+            .wb_data_rvalid   ( RISCV_CORE.data_rvalid_i                        ),
+            .wb_data_rdata    ( RISCV_CORE.data_rdata_i                         )
+          );
+        end
+   endgenerate
+   `endif
+
+endmodule // tb_riscv_core
   
   );
   
